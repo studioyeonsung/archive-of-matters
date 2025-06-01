3ffdf7cd42a0689d42580d88d03bdea1a3a176a8
studioyeonsung <106752807+studioyeonsung@users.noreply.github.com>
2025-06-01 18:59:39 +0200

    1

--- Matter_Copper/script.js
+++ Matter_Copper/script.js
@@ -1,24 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // 항상 보여야 함
-  const odyssey = document.getElementById("odysseySection");
-  odyssey.classList.remove("hidden");
+  const copper = document.getElementById("copperSection");
+  copper.classList.remove("hidden");
 
   const icon = document.getElementById("toggleIcon");
-  if (icon) icon.textContent = "▼";
-
-  // ✅ 드롭다운 기본 열림 상태
-  const dropdown = document.getElementById("dropdownContent");
-  if (dropdown) dropdown.style.display = "block";
+  icon.textContent = "▼";
 });
 
-function toggleDropdown() {
-  const dropdown = document.getElementById("dropdownContent");
-  dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-}
-
-function toggleOdyssey() {
-  const odyssey = document.getElementById("odysseySection");
+function toggleCopper() {
+  const copper = document.getElementById("copperSection");
   const icon = document.getElementById("toggleIcon");
-  const isHidden = odyssey.classList.toggle("hidden");
+  const isHidden = copper.classList.toggle("hidden");
   icon.textContent = isHidden ? "▲" : "▼";
 }
